67ca44af3e31a7eab2a7e9b5ef815c8d4f981726
HaydenJamieson96 <32482976+HaydenJamieson96@users.noreply.github.com>
2017-12-21 12:23:46 +0000

    Login VC set up
    Segue's integrated

---
+++ Smack/Utlilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by TheAppExperts on 12/21/17.
+//  Copyright © 2017 TheAppExperts. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "ShowSignUp"
+
